eb52fa612ce201546af762218e3fb437189fd712
Koushik <dhulipalakoushik5@gmail.com>
2026-04-05 17:38:53 +0530

    fix: resolved file import issue (case mismatch)

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,7 +2,7 @@ import Terminal from './Terminal';
 import Safari from './Safari';
 import Resume from './Resume';
 import Finder from './Finder';
-import Text from './text.jsx';
+import Text from './Text';
 import Image from './Image';
 import Contact from './Contact.jsx';
 
